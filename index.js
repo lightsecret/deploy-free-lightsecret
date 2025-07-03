55680b14c969133e2af65cec1b517acd7a0afbca
lightsecret <ilhammusawa355@gmail.com>
2025-07-03 16:48:32 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 app.post("/api/deploy", upload.single("zipfile"), async (req, res) => {
   try {
     const { token, projectName } = req.body;
-    const defaultToken = "CrGHBiBDwemXp8LeTZS3ub70";
+    const defaultToken = "9SmxPRz5FwGOVT6pnhZbX4Jx";
 
     if (!req.file) {
       return res.status(400).json({ error: "File ZIP tidak ditemukan" });
@@ -168,7 +168,7 @@ app.post("/api/deploy", upload.single("zipfile"), async (req, res) => {
 app.get("/api/listdeploy", async (req, res) => {
   console.log("[/api/listdeploy] Request received");
   try {
-    const token = req.query.token || "CrGHBiBDwemXp8LeTZS3ub70";
+    const token = req.query.token || "9SmxPRz5FwGOVT6pnhZbX4Jx";
     console.log("[/api/listdeploy] Using token:", token);
 
     const response = await axios.get("https://api.vercel.com/v6/deployments", {
@@ -201,7 +201,7 @@ app.get("/api/listdeploy", async (req, res) => {
 app.delete("/api/deldeploy", async (req, res) => {
   try {
     const { token, deploymentId } = req.body;
-    const defaultToken = "CrGHBiBDwemXp8LeTZS3ub70";
+    const defaultToken = "9SmxPRz5FwGOVT6pnhZbX4Jx";
 
     const useToken = token || defaultToken;
 
